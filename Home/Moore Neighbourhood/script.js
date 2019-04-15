3f152e5e4b365f5a59f518d47b0f3cb8eec3277f
Tomasz Lewandowski [L] <tlewandster@gmail.com>
2019-04-16 00:42:40 +0200

    finish task solution

--- Home/Moore Neighbourhood/script.js
+++ Home/Moore Neighbourhood/script.js
@@ -1,34 +1,53 @@
 "use strict";
 
 function countNeighbours(data, row, col) {
-       for (let r = row - 1; r <= row + 1; r++) {
-        console.log(data[r]);
+    let count = 0;
+    for (let r = row - 1; r <= row + 1; r++) {
+        if (data[r]) {
+            let colMin = col - 1;
+            let colMax = col + 2;
+            if (colMin < 0) colMin = 0;
+            if (colMax > data[r].length) colMax = data[r].length;
+            let newData = data[r].slice(colMin, colMax);
+            newData.forEach(element => {
+                if (element) count++;
+            })
+        }
     }
-    return 0;
+    if (data[row][col]) count--;
+    return count;
 }
 
 var assert = require('assert');
 
 if (!global.is_checking) {
-    assert.equal(countNeighbours([[1, 0, 0, 1, 0],
-                                  [0, 1, 0, 0, 0],
-                                  [0, 0, 1, 0, 1],
-                                  [1, 0, 0, 0, 0],
-                                  [0, 0, 1, 0, 0]], 1, 2), 3, "1st example");
+    assert.equal(countNeighbours([
+        [1, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0]
+    ], 1, 2), 3, "1st example");
 
-    assert.equal(countNeighbours([[1, 0, 0, 1, 0],
-                                  [0, 1, 0, 0, 0],
-                                  [0, 0, 1, 0, 1],
-                                  [1, 0, 0, 0, 0],
-                                  [0, 0, 1, 0, 0]], 0, 0), 1, "2nd example");
+    assert.equal(countNeighbours([
+        [1, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 1],
+        [1, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0]
+    ], 0, 0), 1, "2nd example");
 
-    assert.equal(countNeighbours([[1, 1, 1],
-                                  [1, 1, 1],
-                                  [1, 1, 1]], 0, 2), 3, "Dense corner");
+    assert.equal(countNeighbours([
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1]
+    ], 0, 2), 3, "Dense corner");
 
-    assert.equal(countNeighbours([[0, 0, 0],
-                                  [0, 1, 0],
-                                  [0, 0, 0]], 1, 1), 0, "Single");
+    assert.equal(countNeighbours([
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0]
+    ], 1, 1), 0, "Single");
 
     console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
 }
