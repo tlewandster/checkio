4bcfecb5f8dcf487d3edbe6b1eb1dc228ac89926
Tomasz Lewandowski [L] <tlewandster@gmail.com>
2019-04-15 23:13:56 +0200

    add new task

---
+++ Home/Moore Neighbourhood/script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+function countNeighbours(data, row, col) {
+       for (let r = row - 1; r <= row + 1; r++) {
+        console.log(data[r]);
+    }
+    return 0;
+}
+
+var assert = require('assert');
+
+if (!global.is_checking) {
+    assert.equal(countNeighbours([[1, 0, 0, 1, 0],
+                                  [0, 1, 0, 0, 0],
+                                  [0, 0, 1, 0, 1],
+                                  [1, 0, 0, 0, 0],
+                                  [0, 0, 1, 0, 0]], 1, 2), 3, "1st example");
+
+    assert.equal(countNeighbours([[1, 0, 0, 1, 0],
+                                  [0, 1, 0, 0, 0],
+                                  [0, 0, 1, 0, 1],
+                                  [1, 0, 0, 0, 0],
+                                  [0, 0, 1, 0, 0]], 0, 0), 1, "2nd example");
+
+    assert.equal(countNeighbours([[1, 1, 1],
+                                  [1, 1, 1],
+                                  [1, 1, 1]], 0, 2), 3, "Dense corner");
+
+    assert.equal(countNeighbours([[0, 0, 0],
+                                  [0, 1, 0],
+                                  [0, 0, 0]], 1, 1), 0, "Single");
+
+    console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
+}
