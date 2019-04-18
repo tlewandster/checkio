cff243f3e992bd48bdadd3398621bf3359d7d5c8
Tomasz Lewandowski [L] <tlewandster@gmail.com>
2019-04-18 23:11:00 +0200

    add new task

---
+++ Home/Long Repeat/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function longRepeat(line) {
+
+}
+
+var assert = require('assert');
+
+if (!global.is_checking) {
+    assert.equal(longRepeat('sdsffffse'), 4, "First")
+    assert.equal(longRepeat('ddvvrwwwrggg'), 3, "Second")
+    console.log('"Run" is good. How is "Check"?');
+}
