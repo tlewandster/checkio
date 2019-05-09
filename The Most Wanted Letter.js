0a13cb72f750e2c6c14e3c833360769f043829d4
Tomasz Lewandowski <tlewandster@gmail.com>
2019-05-09 15:04:27 +0200

    Task in progress...

--- The Most Wanted Letter.js
+++ The Most Wanted Letter.js
@@ -1,10 +1,18 @@
 "use strict";
 
 function mostWanted(data) {
-    return data;
+  const letterCount = {};
+  [...data.toLowerCase()].forEach(letter => {
+    console.log(letter);
+    letterCount[letter] ? letterCount[letter]++ : letterCount[letter] = 1;
+  });
+  return letterCount;
 }
 
-var assert = require('assert');
+console.table(mostWanted("How do you do?"));
+
+
+/* var assert = require('assert');
 
 if (!global.is_checking) {
     assert.equal(mostWanted("Hello World!"), "l", "1st example");
@@ -13,4 +21,4 @@ if (!global.is_checking) {
     assert.equal(mostWanted("Oops!"), "o", "4th example");
     assert.equal(mostWanted("AAaooo!!!!"), "a", "Letters");
     console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
-}
+} */
