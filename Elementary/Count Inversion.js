ed68644ca6173d98e6283fa31661c92a6d8c622d
Tomasz Lewandowski <tlewandster@gmail.com>
2019-05-14 18:57:47 +0200

    Finish mission 'Count Inversion'

--- Elementary/Count Inversion.js
+++ Elementary/Count Inversion.js
@@ -1,7 +1,13 @@
 "use strict";
 
-function countInversion(sequence){
-    return 0;
+function countInversion(sequence) {
+    let inv = 0;
+    for (let i = 0; i < sequence.length - 1; i++) {
+        for (let j = i + 1; j < sequence.length; j++) {
+            inv += sequence[i] > sequence[j];
+        }
+    }
+    return inv;
 }
 
 var assert = require('assert');
@@ -9,7 +15,7 @@ var assert = require('assert');
 if (!global.is_checking) {
     console.log("Example:");
     console.log(countInversion([1, 2, 5, 3, 4, 7, 6]));
-    
+
     assert.equal(countInversion([1, 2, 5, 3, 4, 7, 6]), 3, "Example");
     assert.equal(countInversion([0, 1, 2, 3]), 0, "Sorted");
     assert.equal(countInversion([99, -99]), 1, "Two numbers");
