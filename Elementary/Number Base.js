c2044b36ba453d8111e4adf064bf7ccb0f6c3cfe
Tomasz Lewandowski <tlewandster@gmail.com>
2019-05-16 00:07:52 +0200

    Mission 'Number Base' in progress...

--- Elementary/Number Base.js
+++ Elementary/Number Base.js
@@ -1,10 +1,15 @@
 "use strict";
 
-function numberRadix(str_number, radix){
-    return -1;
+function numberRadix(str_number, radix) {
+    const strTable = [...'0123456789ABCDEFGHIJKLMNOPQRSTUWVXYZ'];
+    return [...str_number]
+        .reverse()
+        .reduce((acc, val, index) => acc + strTable.indexOf(val) * Math.pow(radix, index), 0);
 }
 
-var assert = require('assert');
+console.log(numberRadix("AB", 10));
+
+/* var assert = require('assert');
 
 if (!global.is_checking) {
     assert.equal(numberRadix("AF", 16), 175, "Hex");
@@ -13,4 +18,4 @@ if (!global.is_checking) {
     assert.equal(numberRadix("Z", 36), 35, "Z base");
     assert.equal(numberRadix("AB", 10), -1, "B > A > 10");
     console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
-}
+} */
