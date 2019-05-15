c4d65babdc0bf3c61a30443ba20aab28fb9b767a
Tomasz Lewandowski <tlewandster@gmail.com>
2019-05-15 20:46:55 +0200

    Add mission 'First Word (simplified)'

---
+++ Elementary/First Word (simplified).js
@@ -0,0 +1,20 @@
+"use strict";
+
+function firstWord(a, b) {
+    // returns the first word in a given text.
+
+    // your code here
+    return 0;
+}
+
+var assert = require('assert');
+if (!global.is_checking) {
+    console.log('Example:')
+    console.log(firstWord("Hello world"))
+
+    // These "asserts" using for self-checking and not for auto-testing
+    assert.equal(firstWord("Hello world"), "Hello")
+    assert.equal(firstWord("a word"), "a")
+    assert.equal(firstWord("hi"), "hi")
+    console.log("Coding complete? Click 'Check' to earn cool rewards!");
+}
