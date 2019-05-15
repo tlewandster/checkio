9949167898bc10edf35c517949a476a2c3599457
Tomasz Lewandowski <tlewandster@gmail.com>
2019-05-15 13:31:58 +0200

    Finish mission 'Bigger Price'

--- Elementary/Bigger Price.js
+++ Elementary/Bigger Price.js
@@ -1,35 +1,69 @@
 "use strict";
 
 function biggerPrice(limit, data) {
-    // TOP most expensive goods
-
-    // your code here
-    return 0;
+  return data
+    .sort((a, b) => b.price - a.price)
+    .filter((v, i) => i < limit);
 }
 
 var assert = require('assert');
 
 if (!global.is_checking) {
-    console.log('Example:')
-    console.log(biggerPrice(2, [
-        {"name": "bread", "price": 100},
-        {"name": "wine", "price": 138},
-        {"name": "meat", "price": 15},
-        {"name": "water", "price": 1}
-    ]))
+  console.log('Example:')
+  console.log(biggerPrice(2, [{
+      "name": "bread",
+      "price": 100
+    },
+    {
+      "name": "wine",
+      "price": 138
+    },
+    {
+      "name": "meat",
+      "price": 15
+    },
+    {
+      "name": "water",
+      "price": 1
+    }
+  ]))
 
-    assert.deepEqual(biggerPrice(2, [
-        {"name": "bread", "price": 100},
-        {"name": "wine", "price": 138},
-        {"name": "meat", "price": 15},
-        {"name": "water", "price": 1}
-    ]), [
-        {"name": "wine", "price": 138},
-        {"name": "bread", "price": 100}
-    ])
-    assert.deepEqual(biggerPrice(1, [
-        {"name": "pen", "price": 5},
-        {"name": "whiteboard", "price": 170}
-    ]), [{"name": "whiteboard", "price": 170}])
-    console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
+  assert.deepEqual(biggerPrice(2, [{
+      "name": "bread",
+      "price": 100
+    },
+    {
+      "name": "wine",
+      "price": 138
+    },
+    {
+      "name": "meat",
+      "price": 15
+    },
+    {
+      "name": "water",
+      "price": 1
+    }
+  ]), [{
+      "name": "wine",
+      "price": 138
+    },
+    {
+      "name": "bread",
+      "price": 100
+    }
+  ])
+  assert.deepEqual(biggerPrice(1, [{
+      "name": "pen",
+      "price": 5
+    },
+    {
+      "name": "whiteboard",
+      "price": 170
+    }
+  ]), [{
+    "name": "whiteboard",
+    "price": 170
+  }])
+  console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
 }
