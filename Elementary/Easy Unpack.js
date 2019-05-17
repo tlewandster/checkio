2baba953fb8e5489b505264585a75077c7935be4
Tomasz Lewandowski <tlewandster@gmail.com>
2019-05-17 09:02:54 +0200

    Finish mission 'Easy Unpack'

--- Elementary/Easy Unpack.js
+++ Elementary/Easy Unpack.js
@@ -1,10 +1,7 @@
 "use strict";
 
 function easyUnpack(elements) {
-    // returns a tuple with 3 elements - first, third and second to the last
-
-    // your code here
-    return 0;
+    return [elements[0], elements[2], elements[elements.length - 2]]
 }
 
 var assert = require('assert');
@@ -12,7 +9,7 @@ var assert = require('assert');
 if (!global.is_checking) {
     console.log('Examples:')
     console.log(easyUnpack([1, 2, 3, 4, 5, 6, 7, 9]))
-    
+
     assert.deepEqual(easyUnpack([1, 2, 3, 4, 5, 6, 7, 9]), [1, 3, 7])
     assert.deepEqual(easyUnpack([1, 1, 1, 1]), [1, 1, 1])
     assert.deepEqual(easyUnpack([6, 3, 7]), [6, 7, 3])
