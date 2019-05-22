ca3ae80974a0b00bbaa1710976f2dc485540b48d
Tomasz Lewandowski <tlewandster@gmail.com>
2019-05-22 21:09:24 +0200

    Add mission 'Between Markers'

---
+++ Elementary/Between Markers.js
@@ -0,0 +1,24 @@
+"use strict";
+
+function betweenMarkers(text, begin, end) {
+    // returns substring between two given markers
+
+    // your code here
+    return 0;
+}
+
+var assert = require('assert');
+
+if (!global.is_checking) {
+    console.log('Example:')
+    console.log(betweenMarkers('What is >apple<', '>', '<'), 'apple')
+
+    assert.equal(betweenMarkers('What is >apple<', '>', '<'), 'apple')
+    assert.equal(betweenMarkers("<head><title>My new site</title></head>",
+                                "<title>", "</title>"), 'My new site')
+    assert.equal(betweenMarkers('No[/b] hi', '[b]', '[/b]'), 'No')
+    assert.equal(betweenMarkers('No [b]hi', '[b]', '[/b]'), 'hi')
+    assert.equal(betweenMarkers('No hi', '[b]', '[/b]'), 'No hi')
+    assert.equal(betweenMarkers('No <hi>', '>', '<'), '')
+    console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
+}
