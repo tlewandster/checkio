562ced29fe414e5506caeb36c332d149234f1b5a
Tomasz Lewandowski <tlewandster@gmail.com>
2019-05-23 00:40:42 +0200

    In progress...

--- Elementary/Between Markers.js
+++ Elementary/Between Markers.js
@@ -1,24 +1,31 @@
-"use strict";
+'use strict';
 
 function betweenMarkers(text, begin, end) {
-    // returns substring between two given markers
-
-    // your code here
-    return 0;
+  const re = new RegExp(`${begin}(.*)${end}`);
+  console.log(text.match(re)[1]);
+  return text.match(re)[1];
 }
 
 var assert = require('assert');
 
 if (!global.is_checking) {
-    console.log('Example:')
-    console.log(betweenMarkers('What is >apple<', '>', '<'), 'apple')
+  console.log('Example:');
+  console.log(betweenMarkers('What is >apple<', '>', '<'), 'apple');
 
-    assert.equal(betweenMarkers('What is >apple<', '>', '<'), 'apple')
-    assert.equal(betweenMarkers("<head><title>My new site</title></head>",
-                                "<title>", "</title>"), 'My new site')
-    assert.equal(betweenMarkers('No[/b] hi', '[b]', '[/b]'), 'No')
-    assert.equal(betweenMarkers('No [b]hi', '[b]', '[/b]'), 'hi')
-    assert.equal(betweenMarkers('No hi', '[b]', '[/b]'), 'No hi')
-    assert.equal(betweenMarkers('No <hi>', '>', '<'), '')
-    console.log("Coding complete? Click 'Check' to review your tests and earn cool rewards!");
+  assert.equal(betweenMarkers('What is >apple<', '>', '<'), 'apple');
+  assert.equal(
+    betweenMarkers(
+      '<head><title>My new site</title></head>',
+      '<title>',
+      '</title>'
+    ),
+    'My new site'
+  );
+  assert.equal(betweenMarkers('No[/b] hi', '[b]', '[/b]'), 'No');
+  assert.equal(betweenMarkers('No [b]hi', '[b]', '[/b]'), 'hi');
+  assert.equal(betweenMarkers('No hi', '[b]', '[/b]'), 'No hi');
+  assert.equal(betweenMarkers('No <hi>', '>', '<'), '');
+  console.log(
+    "Coding complete? Click 'Check' to review your tests and earn cool rewards!"
+  );
 }
