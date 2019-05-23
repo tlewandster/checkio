dd1ef210ec86b13a99cd7d21f373a9d80a604a97
Tomasz Lewandowski <tlewandster@gmail.com>
2019-05-24 00:50:19 +0200

    'Roman Numerals' in progress

--- Home/Roman Numerals.js
+++ Home/Roman Numerals.js
@@ -1,15 +1,25 @@
-"use strict";
+'use strict';
 
 function romanNumerals(number) {
-    return "";
+  let romanString = '';
+  for (let i = 0; i < Math.floor(number / 1000); i++) {
+    romanString += 'M';
+    number -= 1000;
+  }
+  return [romanString, number];
 }
 
-var assert = require('assert');
+console.log(romanNumerals(6), 'VI', 'First');
+console.log(romanNumerals(76), 'LXXVI', 'Second');
+console.log(romanNumerals(499), 'CDXCIX', 'Third');
+console.log(romanNumerals(3888), 'MMMDCCCLXXXVIII', 'Forth');
 
-if (!global.is_checking) {
-    assert.equal(romanNumerals(6), 'VI', "First");
-    assert.equal(romanNumerals(76), 'LXXVI', "Second");
-    assert.equal(romanNumerals(499), 'CDXCIX', "Third");
-    assert.equal(romanNumerals(3888), 'MMMDCCCLXXXVIII', "Forth");
-    console.log("Done! Go Check!");
-}
+// var assert = require('assert');
+
+// if (!global.is_checking) {
+//     assert.equal(romanNumerals(6), 'VI', "First");
+//     assert.equal(romanNumerals(76), 'LXXVI', "Second");
+//     assert.equal(romanNumerals(499), 'CDXCIX', "Third");
+//     assert.equal(romanNumerals(3888), 'MMMDCCCLXXXVIII', "Forth");
+//     console.log("Done! Go Check!");
+// }
