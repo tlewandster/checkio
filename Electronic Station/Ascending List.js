a3fb22fb4b47bb6ccf68956158ad6f27540451ac
Tomasz Lewandowski <tlewandster@gmail.com>
2019-05-28 14:58:33 +0200

    Mission 'Ascending List' in progress

--- Electronic Station/Ascending List.js
+++ Electronic Station/Ascending List.js
@@ -1,21 +1,19 @@
-"use strict";
+'use strict';
 
 function isAscending(items) {
-
-    // your code here
-    return true;
+  return items.filter((v, i) => v[i] - v[i + 1] >= 0) ? false : true;
 }
 
 var assert = require('assert');
 if (!global.is_checking) {
-    console.log('Example:')
-    console.log(isAscending([-5, 10, 99, 123456]))
+  console.log('Example:');
+  console.log(isAscending([-5, 10, 99, 123456]));
 
-    // These "asserts" are used for self-checking and not for an auto-testing
-    assert.equal(isAscending([-5, 10, 99, 123456]), true)
-    assert.equal(isAscending([99]), true)
-    assert.equal(isAscending([4, 5, 6, 7, 3, 7, 9]), false)
-    assert.equal(isAscending([]), true)
-    assert.equal(isAscending([1, 1, 1, 1]), false)
-    console.log("Coding complete? Click 'Check' to earn cool rewards!");
+  // These "asserts" are used for self-checking and not for an auto-testing
+  assert.equal(isAscending([-5, 10, 99, 123456]), true);
+  assert.equal(isAscending([99]), true);
+  assert.equal(isAscending([4, 5, 6, 7, 3, 7, 9]), false);
+  assert.equal(isAscending([]), true);
+  assert.equal(isAscending([1, 1, 1, 1]), false);
+  console.log("Coding complete? Click 'Check' to earn cool rewards!");
 }
